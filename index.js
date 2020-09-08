8ab7a86c00cad52b9a6bbb21f25fd91c69c9e8b9
Jane Lane <loan.does@gmail.com>
2020-09-08 14:12:05 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+/*
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+	console.log('requête reçue');
+	res.writeHead(200);
+	res.end('Coucou');
+});
+
+server.listen(8080);
+
+*/
+
+var http = require('http');
+
+function requestListener(request, response) {
+	response.writeHead(200);
+	response.end('Coucou');
+
+	if (request.method === 'GET') {
+		console.log('requête reçue');
+	} else if (request.method === 'POST') {
+		console.log('no puedo');
+	} else if (request.method != 'GET' && request.method != 'POST') {
+		console.log('Me gusta Alya');
+	}
+};
+
+var server = http.createServer(requestListener);
+
+server.listen(8080);
